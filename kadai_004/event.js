cc4d670ef6b9c02940ee1bb25083adf2827151a9
makimamu <156180587+makimamu@users.noreply.github.com>
2024-08-01 03:57:20 +0900

    kadai_004追加

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-//画面をスクロールした時の処理
-$(window).on('load',function() {
+
+//画面をした時の処理
+$(window).on('load', function() {
   console.log("loadイベントが発生しました");
 });
 
+$(document).ready(function(){
 //画面をスクロールした時の処理
-$(window).on('scroll',function(){
+$(window).on('scroll', function() {
   console.log("scrollイベントが発生しました");
   });
 });
